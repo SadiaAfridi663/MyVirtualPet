5e40c11839c94b8d632c24a203c25c017ed831c0
Sadia Afridi <sadiaafridi663@gmail.com>
2024-10-13 09:30:30 +0500

    add feed method to handel pet hunger

--- index.js
+++ index.js
@@ -11,4 +11,19 @@ function Pet(name, type, age, happiness, hunger) {
         this.petHunger += 10
         alert(`${this.petName} aged up! Age: ${this.petAge}, Hunger: ${this.petHunger}, Happiness: ${this.petHappiness}.\nDon't forget to feed and play`)
     }
+
+    this.feed = function () {
+        if (this.petHunger <= 0) {
+            this.petHunger = 0
+            alert(`${this.petName} is full! No need to feed it right now`)
+        }
+        else if(this.petHunger === 10){
+            this.petHunger -= 10
+            alert(`You've fed your pet. Hunger level: ${this.petHunger} `)
+        }else{
+            this.petHunger -= 20
+            alert(`You've fed your pet. Hunger level: ${this.petHunger} `)
+        }
+    }
+   
 }
