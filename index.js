886358cd556fe73da89cbf321e738693d410d8e3
Sadia Afridi <sadiaafridi663@gmail.com>
2024-10-20 15:09:56 +0500

    add checks for handling hunger/happiness level

--- index.js
+++ index.js
@@ -1,81 +1,83 @@
-function Pet(name, type, age, happiness, hunger) {
-    this.petName = name;
-    this.petType = type;
-    this.petAge = age;
-    this.petHappiness = happiness;
-    this.petHunger = hunger;
+var virtualPet = {
 
-    this.agePet = function () {
-        this.petAge += 1
-        this.petHappiness -= 5
-        this.petHunger += 10
-        alert(`${this.petName} aged up! Age: ${this.petAge}, Hunger: ${this.petHunger}, Happiness: ${this.petHappiness}.\nDon't forget to feed and play`)
-    }
-    this.feed = function () {
+    petName: prompt("Give your pet a name"),
+    petType: prompt("What kind of pet is it? (e.g., 'dog', 'cat', 'dragon')."),
+    petAge: +prompt("How old is your pet?"),
+    petHappiness: +prompt("How happy is your pet? (a number between 0 and 100)"),
+    petHunger: +prompt("How hungry is your pet? (a number between 0 and 100)"),
+
+    agePet: function () {
+        this.petAge += 1;
+        this.petHappiness -= 5;
+        this.petHunger += 10;
+
+        if (this.petHappiness < 0) {
+            this.petHappiness = 0;
+        }
+
+        if (this.petHunger > 100) {
+            this.petHunger = 100;
+        }
+
+        alert(`${this.petName} is growing up! Now ${this.petName} is ${this.petAge} years old. But ${this.petName} is getting hungry (Hunger: ${this.petHunger}) and feeling a bit down (Happiness: ${this.petHappiness}). Time to feed and play!`);
+    },
+
+    feed: function () {
         if (this.petHunger <= 0) {
-            this.petHunger = 0
-            alert(`${this.petName} is full! No need to feed it right now`)
+            this.petHunger = 0;
+            alert(`${this.petName} is already full and can't eat more! No need to feed ${this.petName} right now.`);
+        } else if (this.petHunger <= 10) {
+            this.petHunger -= 10;
+            alert(`${this.petName} enjoyed a  meal! Hunger level is now ${this.petHunger}. Keep an eye on it!`);
+        } else {
+            this.petHunger -= 20;
+            alert(`${this.petName} enjoyed a  meal! Hunger level is now ${this.petHunger}. Keep an eye on it!`);
         }
-        else if(this.petHunger === 10){
-            this.petHunger -= 10
-            alert(`You've fed your pet. Hunger level: ${this.petHunger} `)
-        }else{
-            this.petHunger -= 20
-            alert(`You've fed your pet. Hunger level: ${this.petHunger} `)
-        }
-    }
-    this.play = function () {
+    },
+
+    play: function () {
         if (this.petHappiness >= 100) {
-            this.petHappiness = 100
-            alert("Your pet is feeling great")
-        }else if(this.petHunger === 95){
-            this.petHappiness += 5
-            alert(`You've played with your pet. Happiness level: ${this.petHappiness} `)
-        }
-        else {
-            this.petHappiness += 10
-            alert(`Your pet's happiness has increased to: ${this.petHappiness}. Keep playing for more fun!} `)
+            this.petHappiness = 100;
+            alert(`${this.petName} is already super happy`);
+        } else if (this.petHappiness === 95) {
+            this.petHappiness += 5;
+            alert(`${this.petName} loved playing with you! Happiness is now at ${this.petHappiness}. Keep playing to make ${this.petName} even happier!`);
+        } else {
+            this.petHappiness += 10;
+            alert(`${this.petName} loved playing with you! Happiness is now at ${this.petHappiness}. Keep playing to make ${this.petName} even happier!`);
         }
     }
-}
+};
 
-var petNameInput = prompt("Give your pet a name")
-var petTypeInput = prompt("What kind of pet is it? (e.g., 'dog', 'cat', 'dragon').")
-var petAgeInput = +prompt("How old is your pet?")
-var petHappinessInput = +prompt("How happy is your pet? (a number between 0 and 100)")
-var petHungerInput = +prompt("How hungry is your pet? (a number between 0 and 100)")
-
-var myPet = new Pet(petNameInput, petTypeInput, petAgeInput, petHappinessInput, petHungerInput)
-console.log(`petName:${myPet.petName}, petType: ${myPet.petType},  petAge:,  petType: ${myPet.petType} ${myPet.petAge},  petHappiness: ${myPet.petHappiness},  petHunger: ${myPet.petHunger} `)
-
-var action = 0; 
+var action = 0;
 while (action !== 4) {
 
-    var action = +prompt(
-        `what would you like to do with ${myPet.petName}? \n 
-    1. Feed your pet \n
-    2. Play with your pet \n
-    3. Age your pet \n
+    action = +prompt(
+        `What would you like to do with ${virtualPet.petName}? \n 
+    1. Feed ${virtualPet.petName} \n
+    2. Play with ${virtualPet.petName} \n
+    3. Age ${virtualPet.petName} \n
     4. Quit
-    `)
+    `);
+
     switch (action) {
         case 1:
-            myPet.feed()
+            virtualPet.feed();
             break;
 
-        case 1:
-            myPet.feed()
-            break;
         case 2:
-            myPet.play()
+            virtualPet.play();
             break;
+
         case 3:
-            myPet.agePet()
+            virtualPet.agePet();
             break;
+
         case 4:
-            alert(`${myPet.petName} had a great time with you! See you next time!`)
+            alert(`${virtualPet.petName} enjoyed spending time with you! See you soon!`);
             break;
+
         default:
-            alert("Invalid choice. Please select 1, 2, 3, or 4.")
+            alert("Invalid choice. Please choose 1, 2, 3, or 4.");
     }
 }
